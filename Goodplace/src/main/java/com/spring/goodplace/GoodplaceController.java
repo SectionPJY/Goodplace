29914b46bf6f8a844248471e7bb8e46a152152df
401ST000 <401ST000@401ST022>
2022-05-18 10:20:57 +0900

    SignUp Function Modify

--- Goodplace/src/main/java/com/spring/goodplace/GoodplaceController.java
+++ Goodplace/src/main/java/com/spring/goodplace/GoodplaceController.java
@@ -58,10 +58,13 @@ public class GoodplaceController {
 	@Autowired
 	private LoginCheckCommand loginCheckCommand;
 
+	@Autowired
+	private SignUpCheckCommand signupCheckCommand;
+
 	// 메인페이지
 	@RequestMapping("/index")
 	public String index(Model model) {
-		System.out.println("===== Load Index Page =====");
+		System.out.println("========== Load Index Page ==========");
 
 		return "/index";
 	}
@@ -69,7 +72,7 @@ public class GoodplaceController {
 	// DB 연결 확인용 리스트 출력
 	@RequestMapping("/list")
 	public String list(Model model) {
-		System.out.println("===== List =====");
+		System.out.println("========== List ==========");
 		listCommand.execute(model);
 
 		return "/list";
@@ -85,7 +88,7 @@ public class GoodplaceController {
 	// 로그인 확인
 	@RequestMapping("/loginCheck")
 	public String loginCheck(HttpServletRequest request, Model model) {
-		System.out.println("===== Login Checking =====");
+		System.out.println("========== Login Checking ==========");
 		model.addAttribute("request", request);
 
 		int result = loginCheckCommand.execute(model);
@@ -96,27 +99,35 @@ public class GoodplaceController {
 			return "redirect:login";
 		}
 	}
-	
+
 	// 로그인 완료
 	@RequestMapping("/loginOk")
 	public String loginOk() {
-		
+
 		return "/loginOk";
 	}
 
 	// 회원가입 페이지
 	@RequestMapping("/signup")
 	public String signup() {
-		System.out.println("===== SignUp =====");
+		System.out.println("========== SignUp ==========");
 
 		return "/signup";
 	}
-	
+
 	// 회원가입 확인
 	@RequestMapping("/signupCheck")
 	public String signupCheck(HttpServletRequest request, Model model) {
-		
-		return "";
+		System.out.println("========== SignUp Checking ==========");
+		model.addAttribute("request", request);
+
+		int result = signupCheckCommand.execute(model);
+
+		if (1 == result) {
+			return "redirect:login";
+		} else {
+			return "redirect:signup";
+		}
 	}
 
 }
